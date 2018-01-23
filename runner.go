a988e9ee1a419f77d43381938633f96d777658f1
chvck <chvckd@gmail.com>
2018-01-23 21:51:59 +0000

    Add logic to pick up on incorrect data in runner

--- runner.go
+++ runner.go
@@ -1,11 +1,23 @@
 package romanumeral
 
+import (
+	"fmt"
+)
+
 // Runner is called from main and is resposible for running the application
 type Runner struct {
 }
 
 // Run runs the application
 func (r *Runner) Run(args ...interface{}) (*int, error) {
-	num := 0
-	return &num, nil
+	if len(args) != 1 {
+		return nil, fmt.Errorf("incorrect number of arguments passed")
+	}
+
+	if _, ok := args[0].(int); !ok {
+		return nil, fmt.Errorf("number passed must be an integer number")
+	} else {
+		num := 0
+		return &num, nil
+	}
 }
