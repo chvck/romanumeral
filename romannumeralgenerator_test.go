80c821c65491697ce784b3551db48eab8c6d9e01
chvck <chvckd@gmail.com>
2018-01-23 20:03:00 +0000

    Add tests for remaining symbols

--- romannumeralgenerator_test.go
+++ romannumeralgenerator_test.go
@@ -77,3 +77,63 @@ func TestGenerateWhen1000ThenM(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, "M", rNumeral)
 }
+
+// When 4 is provided then IV should be the value
+func TestGenerateWhen4ThenIV(t *testing.T) {
+	generator := romanumeral.NewRomanNumeralGenerator()
+
+	rNumeral, err := generator.Generate(4)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "IV", rNumeral)
+}
+
+// When 9 is provided then IX should be the value
+func TestGenerateWhen9ThenIX(t *testing.T) {
+	generator := romanumeral.NewRomanNumeralGenerator()
+
+	rNumeral, err := generator.Generate(9)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "IX", rNumeral)
+}
+
+// When 40 is provided then XL should be the value
+func TestGenerateWhen40ThenXL(t *testing.T) {
+	generator := romanumeral.NewRomanNumeralGenerator()
+
+	rNumeral, err := generator.Generate(40)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "XL", rNumeral)
+}
+
+// When 90 is provided then XC should be the value
+func TestGenerateWhen90ThenXC(t *testing.T) {
+	generator := romanumeral.NewRomanNumeralGenerator()
+
+	rNumeral, err := generator.Generate(90)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "XC", rNumeral)
+}
+
+// When 400 is provided then CD should be the value
+func TestGenerateWhen400ThenCD(t *testing.T) {
+	generator := romanumeral.NewRomanNumeralGenerator()
+
+	rNumeral, err := generator.Generate(400)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "CD", rNumeral)
+}
+
+// When 900 is provided then CM should be the value
+func TestGenerateWhen900ThenCM(t *testing.T) {
+	generator := romanumeral.NewRomanNumeralGenerator()
+
+	rNumeral, err := generator.Generate(900)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "CM", rNumeral)
+}
